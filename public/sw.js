5b01382890c363c84a48640da9fd6dcd68e6446d
Snarpxx <vfrancelaurence@gmail.com>
2025-09-13 20:38:37 +0800

    updates

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,19 @@ const STATIC_ASSETS = [
   '/manifest.json',
   '/_next/static/css/app/layout.css',
   '/_next/static/css/app/globals.css',
-  '/favicon.ico'
+  '/favicon.ico',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-180x180.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+  '/icons/shortcut-add.png',
+  '/icons/shortcut-dashboard.png',
+  '/icons/shortcut-chat.png'
 ];
 
 // API routes that should be cached
